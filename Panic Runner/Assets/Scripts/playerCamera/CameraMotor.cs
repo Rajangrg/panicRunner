88765ef4e80036a96b7daf33b832d8ab18833ad9
Rajan G <rajangrg1@hotmail.com>
2020-04-15 21:57:22 +1200

    player run and jump finised

---
+++ Panic Runner/Assets/Scripts/playerCamera/CameraMotor.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMotor : MonoBehaviour
+{
+    public Transform lookAt; // looks at character
+    private Vector3 startOffSet = new Vector3(0,2.0f,-8.0f); 
+    private Vector3 moveVector;
+
+    private float transition = 0.0f;
+    private float animationDuration = 3.0f;
+    private Vector3 animationOffset = new Vector3(0,5.0f,5.0f); //5 meter closer
+
+    private void Start()
+    {
+        transform.position = lookAt.position + startOffSet;
+    }
+
+    private void LateUpdate()
+    {
+        
+        Vector3 desirePosition = lookAt.position + startOffSet;
+        desirePosition.x = 0;
+        transform.position = Vector3.Lerp(transform.position, desirePosition, Time.deltaTime);
+    }
+
+
+
+    // Start is called before the first frame update
+  /*  void Start()
+    {
+        lookAt = GameObject.FindGameObjectWithTag("Player").transform;//find player
+        startOffSet = transform.position - lookAt.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveVector = lookAt.position + startOffSet;
+        //x
+        moveVector.x = 0; //always center
+        //y
+        moveVector.y = Mathf.Clamp(moveVector.y, 2, 5); //restrict from border specifc height(jump change)
+
+      
+        if(transition > 1.0f)
+        {
+            transform.position = moveVector; //no animation
+        }
+        else
+        {
+            //animation at the start of game
+            transform.position = Vector3.Lerp(moveVector + animationOffset, moveVector, transition);
+            transition += (Time.deltaTime * 1) / animationDuration;
+            transform.LookAt(lookAt.position + Vector3.up);
+ 
+
+        }
+
+    }*/
+}
--- Panic Runner/Assets/Scripts/playerMoment/PlayerMotor.cs
+++ Panic Runner/Assets/Scripts/playerMoment/PlayerMotor.cs
@@ -5,22 +5,37 @@ using UnityEngine;
 public class PlayerMotor : MonoBehaviour
 {
     private CharacterController controller;
-    private Vector3 moveCharacter;
-
     private float characterSpeed = 5.0f; //5meter per frame
-    private float verticalVelocity = 0.0f;
+    private float jumpForce = 4.0f;
     private float gravity = 12.0f;
 
+    private float animationDuration = 3.0f;
+    private int desiredLane = 1; //0 = left 1 = middle & 2 = right
+    private float verticalVelocity;
+    private const float LANE_DISTANCE = 3.0f;
+    private const float TURN_SPEED = 0.05f;
+
+    private Animator anim;
+
+    private bool jump = false;
 
     // Start is called before the first frame update
-    public void Start()
+    private void Start()
     {
         controller = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
-    public void Update()
+   /* private void Update()
     {
+        if(Time.time < animationDuration) //right now time is less than 3
+        {
+            controller.Move(Vector3.forward * characterSpeed * Time.deltaTime);
+            return; //and stop update
+        }
+
+        //more than 3 
         if (controller.isGrounded)
         {
             verticalVelocity = -0.5f;
@@ -42,5 +57,94 @@ public class PlayerMotor : MonoBehaviour
 
 
         controller.Move((moveCharacter) * Time.deltaTime); //equal speed on all devices
+    }*/
+
+
+    private void Update()
+    {
+     
+
+        //gather input where we should be
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            MoveLane(false); // not going right so false Move left
+
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            MoveLane(true);//Move right
+        }
+
+        //calulate  where we should be 
+       Vector3 targetPosition = transform.position.z * Vector3.forward;
+
+        if(desiredLane == 0)
+        {
+            targetPosition += Vector3.left * LANE_DISTANCE;
+        }
+        else if (desiredLane == 2)
+        {
+            targetPosition += Vector3.right * LANE_DISTANCE;
+        }
+
+        //cal move delta
+        Vector3 moveCharacter = Vector3.zero;
+        moveCharacter.x = (targetPosition - transform.position).normalized.x * characterSpeed;
+
+        
+
+
+        //calcualte y
+        if (IsGrounded())
+        {
+            verticalVelocity = -0.1f;
+        
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+               
+                verticalVelocity = jumpForce;
+             
+            }
+        }
+        else
+        {
+          
+            verticalVelocity -= (gravity * Time.deltaTime);
+            //fast fall
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                verticalVelocity = -jumpForce;
+                
+            }
+        }
+
+        moveCharacter.y = verticalVelocity;
+        moveCharacter.z = characterSpeed;
+
+
+        //move the character
+        controller.Move(moveCharacter * Time.deltaTime);
+
+        //rotate the character based on the direction
+        Vector3 direction = controller.velocity;
+        if(direction != Vector3.zero)
+        {
+            direction.y = 0;
+            transform.forward = Vector3.Lerp(transform.forward, direction, TURN_SPEED);
+        }
+    }
+
+    private void MoveLane(bool goingRight)
+    {
+        desiredLane += goingRight ? 1 : -1; //add one if right and minus on less
+        desiredLane = Mathf.Clamp(desiredLane, 0, 2); //border restriction bcoz right is 2
+    }
+
+    private bool IsGrounded()
+    {
+        Ray groundray = new Ray(new Vector3(controller.bounds.center.x, (controller.bounds.center.y - controller.bounds.extents.y) + 0.2f, controller.bounds.center.z), Vector3.down);
+        Debug.DrawRay(groundray.origin, groundray.direction, Color.cyan, 1.0f);
+        return Physics.Raycast(groundray, 0.2f + 0.1f);
     }
 }
